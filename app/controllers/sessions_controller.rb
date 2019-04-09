af9e941484569b165f781d0abdc6228403abda65
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Account activation Task 10279

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     if @user&.authenticate(params[:session][:password])
-      log_in @user
-      if params[:session][:remember_me] == Settings.remember_me
-        remember @user
-      else
-        forget @user
-      end
-      redirect_to @user
+      user_activated_or_not @user
     else
       flash.now[:danger] = t ".invalid_email_password"
       render :new
@@ -26,5 +20,22 @@ class SessionsController < ApplicationController
 
   def load_user
     @user = User.find_by email: params[:session][:email].downcase
+    return if @user
+    flash[:danger] = t "controllers.sessions.user_not_found"
+    redirect_to root_path
+  end
+
+  def user_activated_or_not user
+    if user.activated?
+      log_in user
+      if params[:session][:remember_me] == Settings.remember_me
+        remember user
+      else
+        forget user
+      end
+    else
+      flash[:warning] = t ".account_not_activated"
+      redirect_to root_path
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,20 @@ User.create!(
   name: "Admin",
   email: "lyld1203@gmail.com",
   password: "123456",
-  password_confirmation: "123456"
+  password_confirmation: "123456",
+  activated: true,
+  activated_at: Time.zone.now
 )
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    activated: true,
+    activated_at: Time.zone.now
+  )
+end
