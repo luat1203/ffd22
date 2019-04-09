88551d205b056e649e16406929b0b807cc3193bc
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Advanced login Task 10620

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_064500) do
+ActiveRecord::Schema.define(version: 2019_04_11_013943) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_064500) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
