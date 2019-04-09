f342b4ed636aa0bfa7590d53615ec643ca1e6ca3
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Add products and show cart Task #11288

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,9 @@
 module ProductsHelper
   def categories_ordered
-    Category.order(:name).collect{|c| [c.name, c.id]}
+    Category.order(:name).map{|c| [c.name, c.id]}
+  end
+
+  def truncate_info info
+    info.truncate(Settings.product_information.truncate, separator: " ")
   end
 end
