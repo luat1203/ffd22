cdac0a1145f38ef33ed592b45546bc81364e8479
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Task#10935 Cart update delete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,19 +1,38 @@
 class CartsController < ApplicationController
   require "json"
   before_action :logged_in_user, :load_cart_products,
-    only: %i(create show)
+    except: %i(index new edit)
   before_action :load_item, only: :create
+  before_action :check_item_update , only: :update
 
   def create
     @cart_products.merge!(@item)
     cookies.permanent[current_user.id] = JSON.generate(@cart_products)
     respond_to do |format|
-      format.html{redirect_to root_path}
+      format.html{redirect_to products_path}
       format.js
     end
   end
 
   def show; end
+
+  def update
+    @cart_products[params[:carts][:product_id]] = params[:carts][:quantity]
+    cookies.permanent[current_user.id] = JSON.generate(@cart_products)
+    respond_to do |format|
+      format.html{redirect_to carts_path}
+      format.js
+    end
+  end
+
+  def destroy
+    @cart_products.delete params[:product_id]
+    cookies.permanent[current_user.id] = JSON.generate(@cart_products)
+    respond_to do |format|
+      format.html{redirect_to carts_path}
+      format.js
+    end
+  end
 
   private
 
@@ -24,20 +43,28 @@ class CartsController < ApplicationController
   def load_item
     product_id = params[:cart][:product_id]
     quantity = params[:cart][:quantity]
-    check_item_present product_id, quantity
-    if @cart_products[product_id].present?
-      quantity = @cart_products[product_id].to_i + quantity.to_i
-    end
-    @item = {product_id => quantity}
-  end
-
-  def check_item_present product_id, quantity
-    if Product.find_by(id: product_id).blank?
-      flash[:danger] = t "controllers.carts.product_not_found"
+    if check_item_available product_id, quantity
+      if @cart_products[product_id].present?
+        quantity = @cart_products[product_id].to_i + quantity.to_i
+      end
+      @item = {product_id => quantity}
+    else
+      flash[:danger] = t "controllers.carts.have_error"
       redirect_to products_path
     end
-    return if quantity >= Settings.carts.minimum_quantity.to_s
-    flash[:danger] = t "controllers.carts.must_greater_1"
-    redirect_to products_path
+  end
+
+  def check_item_update
+    return if check_item_available [params[:carts][:product_id]],
+      params[:carts][:quantity]
+    flash[:danger] = t "controllers.carts.have_error"
+    redirect_to carts_path
+  end
+
+  def check_item_available product_id, quantity
+    product = Product.find_by(id: product_id)
+    return false if product.blank? || quantity < Settings.carts.minimum_quantity.to_s ||
+      product.quantity < quantity.to_i
+    return true
   end
 end
--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,9 +1,6 @@
 module CartsHelper
   def find_product id
-    product = Product.find_by id: id
-    return product if product
-    flash[:danger] = t "controllers.carts.product_not_found"
-    redirect_to products_path
+    Product.find_by id: id
   end
 
   def cart_products
@@ -14,5 +11,12 @@ module CartsHelper
 
   def render_cart_quantity
     cart_products.blank? ? Settings.carts.no_product : cart_products.size
+  end
+
+  def calculate_total_price cart_products
+    cart_products.sum do |id, quantity|
+      product = find_product id
+      product.present? ? product.price.to_i * quantity.to_i : 0
+    end
   end
 end
