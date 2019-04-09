4b0f63f3056da25f8ee9dbda4337245ac1c26b1e
Luat Hoang <luat1203@gmail.com>
2019-04-09 09:56:36 +0700

    Password reset Task 10283

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
 
+    get "password_resets/new"
+    get "password_resets/edit"
     get "static_pages/home"
     get "/signup", to: "users#new"
     get "/login", to: "sessions#new"
@@ -10,5 +12,6 @@ Rails.application.routes.draw do
 
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index destroy show)
   end
 end
