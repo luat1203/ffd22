75e445025145f45dacaa109fd9c55b3768a3f6b5
Luat Hoang <luat1203@gmail.com>
2019-04-17 14:30:52 +0700

    Modeling and show product Task 10288

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  belongs_to :category
+  validates :name, presence: true,
+    length: {
+      maximum: Settings.product_name.maximum,
+      minimum: Settings.product_name.minimum
+    }
+  validates :price, numericality: {greater_than: Settings.product_price.minimum}
+  validates :information, presence: true,
+    length: {
+      maximum: Settings.product_information.maximum,
+      minimum: Settings.product_information.minimum
+    }
+  validates :quantity,
+    numericality: {greater_than_or_equal_to: Settings.product_quantity.minimum}
+  validates :category_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,36 @@ User.create!(
     activated_at: Time.zone.now
   )
 end
+
+# Category Seed
+Category.create!(
+  name: "Category 1",
+)
+
+Category.create!(
+  name: "Category 2",
+  parent_id: 1
+)
+
+# Product Seed
+12.times do
+  Product.create!(
+    name: Faker::Lorem.sentence(5),
+    price: Faker::Number.between(10, 900),
+    information: Faker::Lorem.paragraph(5),
+    classify: true,
+    quantity: Faker::Number.between(1, 90),
+    category_id: 1
+  )
+end
+
+10.times do
+  Product.create!(
+    name: Faker::Lorem.sentence(5),
+    price: Faker::Number.between(10, 900),
+    information: Faker::Lorem.paragraph(5),
+    classify: false,
+    quantity: Faker::Number.between(1, 90),
+    category_id: 2
+  )
+end
