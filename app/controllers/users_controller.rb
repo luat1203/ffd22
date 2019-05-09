9332522b432a4bb72f031bbb2aafad983f31191f
Luat Hoang <luat1203@gmail.com>
2019-05-09 11:14:35 +0700

    Using gem devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,12 @@
 class UsersController < ApplicationController
-  before_action :load_user, except: %i(index new create)
-  before_action :logged_in_user, except: %i(show create new)
-  before_action :correct_user, only: %i(edit update)
-  before_action :admin_user, only: :destroy
+  before_action :load_user, only: :show
+  before_action :authenticate_user!, only: %i(index show)
 
   def index
     @users = User.paginate page: params[:page], per_page: Settings.per_page
   end
 
-  def new
-    @user = User.new
-  end
-
   def show; end
-
-  def create
-    @user = User.new user_params
-    if @user.save
-      @user.send_activation_email
-      flash[:info] = t ".check_email"
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @user.update_attributes user_params
-      flash[:success] = t ".profile_updated"
-      redirect_to @user
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    if @user.destroy
-      flash[:success] = t ".user_deleted"
-    else
-      flash[:danger] = t ".delele_failed"
-    end
-    redirect_to :users
-  end
 
   private
 
@@ -52,14 +15,5 @@ class UsersController < ApplicationController
     return if @user
     flash[:danger] = t ".load_user.user_not_found"
     redirect_to :root
-  end
-
-  def user_params
-    params.require(:user).permit :name, :email, :password,
-      :password_confirmation, :phone
-  end
-
-  def correct_user
-    redirect_to root_path unless current_user? @user
   end
 end
